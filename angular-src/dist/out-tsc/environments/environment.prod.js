83dd2bef8fad62c3aef8eda926d79f3373b2417f
Malte Brockann <ga46siz@mytum.de>
2017-06-07 23:48:29 +0200

    Starting Point

---
+++ angular-src/dist/out-tsc/environments/environment.prod.js
@@ -0,0 +1,4 @@
+export var environment = {
+    production: true
+};
+//# sourceMappingURL=C:/Users/Malte/Downloads/meanauthapp_part_8/meanauthapp_part_8/angular-src/src/environments/environment.prod.js.map
