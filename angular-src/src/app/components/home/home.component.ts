77c2d50473dcaf87f4cf6f196a9cfce080822bef
Richard Madl <richard.madl@tum.de>
2017-06-16 13:12:26 +0200

    Changed author and fixed header.

--- angular-src/src/app/components/home/home.component.ts
+++ angular-src/src/app/components/home/home.component.ts
@@ -74,10 +74,10 @@ export class HomeComponent implements OnInit {
     this.authService.registerUser(user).subscribe(data => {
       if(data.success){
         this.flashMessage.show('You are now registered and can log in', {cssClass: 'alert-success', timeout: 3000});
-        this.router.navigate(['/login']);
+        location.reload();
+        document.body.scrollTop = 0;
       } else {
         this.flashMessage.show('Something went wrong', {cssClass: 'alert-danger', timeout: 3000});
-        this.router.navigate(['/register']);
       }
     });
 
